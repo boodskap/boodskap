3cf98190160aa0857307b5a861b9f92fa8ee802e
Jegan Vincet <jvincent@boodskap.io>
2021-05-05 09:40:42 +0000

    updated platform to 4.0.0-0010

--- platform/build.sh
+++ platform/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PVERSION="4.0.0"
-PPATCH="0009"
+PPATCH="0010"
 echo "Building with"
 echo "docker build --build-arg PVERSION=${PVERSION} --build-arg PPATCH=${PPATCH} -t boodskapiot/platform:${PVERSION}-${PPATCH} -t boodskapiot/platform:latest ."
 docker build --build-arg PVERSION=${PVERSION} --build-arg PPATCH=${PPATCH} -t boodskapiot/platform:${PVERSION}-${PPATCH} -t boodskapiot/platform:latest .
